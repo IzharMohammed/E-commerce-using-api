1515f355a697919c900118a4d6db08ad0016eceb
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-01-11 23:59:56 +0530

    Added product details,lists and somewhat cart

---
+++ JS/index.js
@@ -0,0 +1,26 @@
+async function allcategories(){
+  const response=  await fetch('https://fakestoreapi.com/products/categories');
+ const data =await response.json();
+return data
+}
+
+async function categoriesDom(){
+    const categories = await allcategories()
+    const categoryRow=document.getElementById('row')
+
+        categories.forEach((element)=> {
+            const div =document.createElement('div')
+            const a = document.createElement('a')
+            div.classList.add('products')
+            a.href=''
+            div.append(a)
+            a.textContent=element
+            categoryRow.append(div)
+        })
+        
+        // document.append(div)
+
+
+}
+
+categoriesDom()
