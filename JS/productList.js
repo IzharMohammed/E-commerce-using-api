e0bc6898c0efc23da2454e000ceaeb486c2f696e
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-01-14 17:59:38 +0530

    added lists of products using fakestore api

--- JS/productList.js
+++ JS/productList.js
@@ -1,17 +1,25 @@
-
+document.addEventListener('DOMContentLoaded', async ()=>{
 
 async function fetchProductList(){
 const response = await axios.get("https://fakestoreapi.com/products")
 return response.data
-
-
-
 }
+
+const fetchproducts = await fetchProductList()
+
+
  async function productList() {
-const data = fetchProductList()
+const products = await fetchProductList()
+
+
+const productListBox= document.getElementById('productList')
+
+const container = document.createElement('div')
+container.classList.add('container')
+
+products.forEach((product) =>{
 
 //creating  elements
-const productListBox= document.createElement('div')
 const imgDiv=document.createElement('div')
 const productName = document.createElement('div')
 const productPrice = document.createElement('div')
@@ -19,16 +27,45 @@ const productDetails = document.createElement('a')
 productDetails.href='productDetails.html'
 
 //adding class name
-productListBox.classList.add('product-list-box')
 imgDiv.classList.add('imgDiv')
 productName.classList.add('product-name')
 productPrice.classList.add('product-price')
+productDetails.classList.add('product-list-item')
+
+console.log(product.title);
+
+productName.textContent=product.title.substring(0,12)+"..."
+productPrice.textContent=product.price
+
+const image = document.createElement('img')
+image.src=product.image
+
+//Attaching according to hardcoded 
+productDetails.appendChild(imgDiv)
+imgDiv.appendChild(image)
+productDetails.appendChild(productName)
+productDetails.appendChild(productPrice)
+
+productListBox.appendChild(productDetails)
+ })
+ document.body.appendChild(container)
+}
+productList()
+})
+
+
+
+const search = document.getElementById('search')
+search.addEventListener('click',()=>{
+
+const minvalue=document.getElementsByClassName('min-range')
+const maxvalue=document.getElementsByClassName('max-price')
+
+console.log();
+
+})
 
 
 
 
 
-
-
- }
- productList()
